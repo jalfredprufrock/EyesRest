acfc8d11c0aaefe5074df5415df74fcbae3a2dc4
George Chronis <g.chronis01@gmail.com>
2016-09-20 23:51:20 -0400

    Update eyes.java

--- eyes.java
+++ eyes.java
@@ -6,15 +6,8 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 public class eyes{
-	/* Rest program, every twenty minutes puts up a screen wide dialog box or an image, and 
-	 * disables user input for twenty seconds, should probably be able to change frequency and duration 
-	 * of screen outs
-	 * NEXT: 
-	 * 1.  get black panel to fade in !!!!
-	 * 2. style it the main frame:  colors, perhaps make it not so shitty looking, i.e. make own graphics, alter existing ones so it looks slick
-	 * 3. put it in icon tray for minimization,  so I would have to create icon graphic, 
-	 * 4. figure out how to get it onto other computers?
-	 * 5. include longer breaks?
+	/* Rest program, every twenty minutes puts up a screen wide window for twenty seconds' duration and 
+	 * disables user input for twenty seconds.
 	 */
 	public static void main(String[] args) {
 		//Schedule a job for the event-dispatching thread:
@@ -26,13 +19,14 @@ public class eyes{
 		});
 	}
 	public static class EyeRest extends JPanel{
+		//responds to stop button, turns timer off
 		boolean stop;
 		public EyeRest() {
 			super(new BorderLayout());
 			
-			//Create the radio buttons.
-			JFrame  black= new JFrame();
-			JLabel label = new JLabel("Stare at something 20 meters away");
+		//Create fullscreen window.
+		JFrame  black= new JFrame();
+		JLabel label = new JLabel("Stare at something 20 meters away");
 	        label.setPreferredSize(new Dimension(500, 500));
 	        label.setForeground(Color.blue);
 	        label.setFont(new Font("Serif", Font.BOLD, 28));
@@ -42,27 +36,30 @@ public class eyes{
 	    	black.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 	    	black.setUndecorated(true);
 	    	black.getContentPane().setBackground(Color.gray);
-	    	
-			JButton onButton = new JButton("On");
-			JButton offButton =new JButton("Off");
+	    	//create buttons to start and stop the program
+		JButton onButton = new JButton("On");
+		JButton offButton =new JButton("Off");
+		//add action listener to on button
 			onButton.addActionListener(new ActionListener(){
 				ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 			        @Override
 			        public void actionPerformed( ActionEvent e ) {
+			        	//if on button is pressed, disable addtional presses
+			        	//enable off button
 			        	onButton.setEnabled(false);
 				        offButton.setEnabled(true);
 			            // add Action
-			        	//System.out.println("ON");
 			        	Runnable blackenRunnable = new Runnable() {
 						    public void run() {
-						    	//make a new screen wide frame that is black or dark grey
-						    	//milliseconds
+						    	//make a new screen wide frame that is black
+						    	//set timer to twenty seconds 
 						    	Timer timer = new Timer(20000, new ActionListener() {
 						    	    public void actionPerformed(ActionEvent a) {
 						    	        black.setVisible(false);
 						    	    }
 						    	});
 						    	if(stop){
+						    		//stop timer and make the executor service wait
 						    		timer.stop();
 						    		stop=false;
 						    		try {
@@ -72,51 +69,48 @@ public class eyes{
 									}
 						    	}
 						    	else
-						    	{
+						    	{	//timer should only run only per scheduled service
 							    	timer.setRepeats(false);
 							    	timer.start();
-							    	//could do set Visible but i'd prefer to actually destroy it
-							    	//black.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 							    	black.setVisible(true);
 							    	black.setAlwaysOnTop(true);
-							    	//black.setBackground(new Color(1,1,1,115));
 						    	}
 						    }
 						};
-						//calls something every number of seconds
-						//condition didn't seem to do anything
+						//calls runnable every twenty minutes
 						if(!stop){
 							executor.scheduleAtFixedRate(blackenRunnable, 0, 1200, TimeUnit.SECONDS);
 						}
 			        }
 			    });
+			    //add action listener to off button
 			offButton.addActionListener(new ActionListener(){
 			        public void actionPerformed( ActionEvent e ) {
+			        	//if off button is clicked, disable further clicks and 
+			        	//enable off button
 			        	onButton.setEnabled(true);
 				        offButton.setEnabled(false);
 				        stop=true;
+				        //get rid of black window 
 			        	black.dispose();
 			        }
 			    });
-			//Group the radio buttons.
+			//Group the buttons.
 			ButtonGroup group = new ButtonGroup();
 			group.add(onButton);
 			group.add(offButton);
 			
-			//Put the radio buttons in a column in a panel.
-			JPanel radioPanel = new JPanel(new GridLayout(10, 1));
-			radioPanel.add(onButton);
-			radioPanel.add(offButton);
+			//Put the buttons in a column in a panel.
+			JPanel panel = new JPanel(new GridLayout(10, 1));
+			panel.add(onButton);
+			panel.add(offButton);
 			
-			add(radioPanel, BorderLayout.LINE_START);
-			//add(picture, BorderLayout.CENTER);
+			add(panel, BorderLayout.LINE_START);
 			setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
 		}
 	
 		/**
-		* Create the GUI and show it.  For thread safety,
-		* this method should be invoked from the
-		* event-dispatching thread.
+		* Create the GUI and show it.
 		*/
 		private static void createAndShowGUI() {
 		//Create and set up the window.
@@ -128,9 +122,11 @@ public class eyes{
 		newContentPane.setOpaque(true); //content panes must be opaque
 		frame.setContentPane(newContentPane);
 		Dimension d = new Dimension(300, 90);
-		//IS THIS THE CORRECT WAY TO SET SIZE UPON OPENING??
-		frame.setMinimumSize(d);
+		frame.setSize(d);
+		//set in middle of screen
 		frame.setLocation(500, 200);
+		//no resizing, only two buttons, don't need much space
+		frame.setResizable(false);
 		//size the frame
 		frame.pack();
 		//Display the window.
